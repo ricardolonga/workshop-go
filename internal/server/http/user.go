f5368b4c916151b596d0e9e220950bae8bde181f
Ricardo Longa <ricardo.longa@gmail.com>
2018-05-24 22:26:34 -0300

    Blank.

--- internal/server/http/user.go
+++ internal/server/http/user.go
@@ -1,23 +1,9 @@
 package http
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
-	"github.com/ricardolonga/workshop-go/domain"
 )
 
 func (h *handler) postUser(c *gin.Context) {
-	user := &domain.User{}
 
-	if err := c.BindJSON(&user); err != nil {
-		return
-	}
-
-	if !h.userService.IsValid(user) {
-		c.AbortWithStatus(http.StatusBadRequest)
-		return
-	}
-
-	c.JSON(http.StatusCreated, nil)
 }
