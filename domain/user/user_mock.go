9ef0d0da0a47d6b0db636c0e3889e5512f410b69
ricardo <ricardo.longa@neoway.com.br>
2018-05-28 10:02:18 -0300

    Mock de UserService nos testes da camada de Controller.

--- domain/user/user_mock.go
+++ domain/user/user_mock.go
@@ -6,9 +6,11 @@ import "github.com/ricardolonga/workshop-go/domain"
 type ServiceMock struct {
 	IsValidFn      func(user *domain.User) bool
 	IsValidInvoked bool
+	IsValidCount   int
 }
 
 func (sm *ServiceMock) IsValid(user *domain.User) bool {
 	sm.IsValidInvoked = true
+	sm.IsValidCount++
 	return sm.IsValidFn(user)
 }
--- internal/server/http/user_test.go
+++ internal/server/http/user_test.go
@@ -6,22 +6,70 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/ricardolonga/workshop-go/domain/user"
 	"github.com/stretchr/testify/assert"
+	"github.com/ricardolonga/workshop-go/domain"
+	"github.com/ricardolonga/workshop-go/domain/user"
+	"fmt"
 )
 
-func TestWithRecorder(t *testing.T) {
-	userService := user.NewService()
+func init() {
+	fmt.Println("init...")
+}
 
-	router := NewHandler(userService)
+func TestMain(m *testing.M) {
+	fmt.Println("antes...")
+	m.Run()
+	fmt.Println("...depois")
+}
 
-	response := httptest.NewRecorder()
-	endpoint := "/v1/users"
-	body := []byte(`{"name": "Ricardo Longa", "age": 31}`)
+func TestController(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		userService := &user.ServiceMock{
+			IsValidFn: func(user *domain.User) bool {
+				assert.Equal(t, "Ricardo Longa", user.Name)
+				assert.Equal(t, 31, user.Age)
 
-	req, _ := http.NewRequest("POST", endpoint, bytes.NewReader(body))
+				return true
+			},
+		}
 
-	router.ServeHTTP(response, req)
+		router := NewHandler(userService)
 
-	assert.Equal(t, http.StatusCreated, response.Code)
+		response := httptest.NewRecorder()
+		endpoint := "/v1/users"
+		body := []byte(`{"name": "Ricardo Longa", "age": 31}`)
+
+		req, _ := http.NewRequest("POST", endpoint, bytes.NewReader(body))
+
+		router.ServeHTTP(response, req)
+
+		assert.Equal(t, http.StatusCreated, response.Code)
+
+		assert.Equal(t, 1, userService.IsValidCount)
+	})
+
+	t.Run("failed", func(t *testing.T) {
+		userService := &user.ServiceMock{
+			IsValidFn: func(user *domain.User) bool {
+				assert.Equal(t, "Ricardo Longa", user.Name)
+				assert.Equal(t, 17, user.Age)
+
+				return false
+			},
+		}
+
+		router := NewHandler(userService)
+
+		response := httptest.NewRecorder()
+		endpoint := "/v1/users"
+		body := []byte(`{"name": "Ricardo Longa", "age": 17}`)
+
+		req, _ := http.NewRequest("POST", endpoint, bytes.NewReader(body))
+
+		router.ServeHTTP(response, req)
+
+		assert.Equal(t, http.StatusBadRequest, response.Code)
+
+		assert.Equal(t, 1, userService.IsValidCount)
+	})
 }
